60fede6ae8b5ada47b4e8a05a09a97413d8f8f7b
cforcross <ntsendifor@gmail.com>
2021-09-21 22:21:01 +0300

    first_commit

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('fear/',views.fear,name='fear'),
+    path('price/',views.price,name='price'),
+]
